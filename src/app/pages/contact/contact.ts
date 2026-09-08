139d2f349f8117e3139dfda9f9a41f60294ef508
Jeeva-1019 <mpjeevanant1996@gmail.com>
2026-09-08 20:31:20 +0530

    Add EmailJS contact form

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   imports: [],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrl: './contact.css'
 })
 export class Contact {
 
+  sendMessage() {
+
+    const form = document.querySelector('#contact-form') as HTMLFormElement;
+
+    emailjs.sendForm(
+      'service_en4xlkq',
+      'template_191cvdn',
+      form,
+      {
+        publicKey: 'w4XI6Kal47mpmmPfZ'
+      }
+    )
+    .then(() => {
+      alert('Message sent successfully! ✅');
+      form.reset();
+    })
+    .catch((error) => {
+      console.error('EmailJS Error:', error);
+      alert('Failed to send message. Please try again.');
+    });
+  }
 }
